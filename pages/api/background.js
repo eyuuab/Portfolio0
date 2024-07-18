c0a30a96c1bfbcd5014720ce2fae195e1e565c87
eyobed <eyobedabreham9@gmail.com>
2024-07-18 22:59:01 +0300

    modify: background and expertise

--- pages/api/background.js
+++ pages/api/background.js
@@ -3,24 +3,24 @@ const background = [
         eduCards: [
             {
                 id: 0,
-                title: 'UET Peshawar',
-                degree: 'BS, Computer System Engineering',
-                detail: "Bachelor's Degree in Computer System Engineering from UET Peshawar.",
-                year: '2018-2022'
+                title: 'Adiss Ababa University',
+                degree: 'BS, Computer Science',
+                detail: "Bachelor's Degree in Computer Science from Adiss Ababa University, Ethiopia.",
+                year: '2021-present'
             },
             {
                 id: 1,
-                title: 'Peshawar Model Degree College',
-                degree: 'HSSC, Pre Engineering',
-                detail: "Completed FSC part 1 and part 2 in Pre-Engineering from Peshawar Model Degree College.",
-                year: '2015-2017'
+                title: 'Yaberus Wolkite High School',
+                degree: '',
+                detail: "Completed High School from Yaberus Wolkite High School, Ethiopia.",
+                year: '2018-2021'
             },
             {
                 id: 2,
-                title: 'Falcon Academy',
-                degree: 'SSC, Science Subjects',
-                detail: "Completed SSC part 1 and part 2 in Science subjects from Falcon Academy Seni Gumbat Kohat",
-                year: '2013-2015'
+                title: "Melketsadik  School",
+                degree: "",
+                detail: "Completed Primary School from Melketsadik School, Ethiopia.",
+                year: '2009-2018'
             },
         ]
     },
@@ -28,31 +28,14 @@ const background = [
         expCards: [
             {
                 id: 1,
-                title: 'JMM Technologies',
-                role: 'Frontend Developer',
-                url: 'https://jmm.ltd/',
+                title: 'iCog Labs',
+                role: 'Software Developer',
+                url: 'https://icog-labs.com/',
                 desc: 'As a frontend developer, I use React, Next & JavaScript to build user interfaces for web applications.',
-                year: '02/2023 - Present',
-                location: 'Peshawar, Pakistan'
+                year: '04/2024 - Present',
+                location: 'Addis Ababa, Ethiopia'
             },
-            {
-                id: 2,
-                title: 'HauzaTech',
-                role: 'Internee',
-                url: 'no website',
-                desc: 'As an Internee, I learned how to use React & JavaScript to build interactive websites.',
-                year: '02/2023 - Present',
-                location: 'Peshawar, Pakistan'
-            },
-            {
-                id: 3,
-                title: 'Encoder Bytes',
-                role: 'PHP Developer',
-                url: 'https://www.encoderbytes.com/',
-                desc: "I work there as a PHP developer, there I learned how to do CRUD'S operations in PHP, also I worked on Firebase",
-                year: '09/2020 - 02/2021',
-                location: 'Peshawar, Pakistan'
-            },
+            
 
         ]
     }
